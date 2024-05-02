3db32cf3b236a178d555e7842d28ce2db4c544f7
Ricardo Gimenes Ferreira <ricardogi@google.com>
2024-05-02 16:28:56 -0300

    trying to implement CSSs

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,18 @@
-import '../css/style.css'
+// import '../css/style.css'
 
 import { TERMS } from './TERMS.js';
 
-  const exitAnimation = 'animate__fadeOutUp',
-    introAnimation = 'animate__fadeInUp',
-    animationStart = "animate__animated",
+  const EXIT_ANIMATION = 'animate__fadeOutUp',
+    INTRO_ANIMATION = 'animate__fadeInUp',
+    START_ANIMATION = "animate__animated",
     controls = {
       trend: document.querySelector('#nova_trend'),
       adj: document.querySelector('#novo_adjetivo'),
       subst: document.querySelector('#novo_substantivo')
     },
-    tempo_ms = 400,
-    adjetivosLength = 4850,
-    substantivosLength = 996,
+    TEMPO_MS = 300,
+    ADJETIVOS_LENGTH = TERMS.adjetivos.length,
+    SUBSTANTIVOS_LENGTH = TERMS.substantivos.length,
     adjetivos_container = document.querySelector('#adjetivos'),
     substantivos_container = document.querySelector('#verbos_substantivo');
   
@@ -21,12 +21,12 @@ import { TERMS } from './TERMS.js';
   function criar_tendencia(is_adjetivo, is_substantivo) {
     let n = 0;
     if (is_adjetivo) {
-      n = returRandom(adjetivosLength);
+      n = returRandom(ADJETIVOS_LENGTH);
       let novo_adjetivo = TERMS.adjetivos[n];
       novo_elemento_dom(adjetivos_container, novo_adjetivo);
     }
     if (is_substantivo) {
-      n = returRandom(substantivosLength);
+      n = returRandom(SUBSTANTIVOS_LENGTH);
       let novo_substantivo = TERMS.substantivos[n];
       novo_elemento_dom(substantivos_container, novo_substantivo);
     }
@@ -35,19 +35,19 @@ import { TERMS } from './TERMS.js';
   
   function novo_elemento_dom(container, innerText) {
     var dom_to_remove = container.querySelector('.j-ativo')
-    dom_to_remove.className = `${animationStart} ${exitAnimation}`;
+    dom_to_remove.className = `${START_ANIMATION} ${EXIT_ANIMATION}`;
     dom_to_remove.addEventListener("animationend", function(e) {
       e.target.remove();
     });
     var dom_palavra = document.createElement('div');
     dom_palavra.innerHTML = innerText;
-    dom_palavra.className = `j-ativo ${animationStart} ' ' ${introAnimation}`;
+    dom_palavra.className = `j-ativo ${START_ANIMATION} ' ' ${INTRO_ANIMATION}`;
     container.appendChild(dom_palavra);
   }
   
   function click_criar_tendencia(e) {
     removeListeners();
-    setTimeout(addListeners, tempo_ms);
+    setTimeout(addListeners, TEMPO_MS);
     switch (e.target.id) {
       case "novo_adjetivo":
         criar_tendencia(true, false);
@@ -76,16 +76,15 @@ import { TERMS } from './TERMS.js';
     }
   }
   criar_tendencia(true, true);
-  setTimeout(addListeners, tempo_ms);
+  setTimeout(addListeners, TEMPO_MS);
   
   var traduzir_br = document.querySelector('#btn_traduzir'),
   spinner = document.querySelector('#spinner-container');
   traduzir_br.addEventListener('click', function(e){
-    console.log(e);
-    spinner.className =  animationStart+' '+introAnimation;
-    var texto = '';
-    var text_parts = document.querySelectorAll('.j-ativo');
-    texto = text_parts[0].textContent+'%20'+ text_parts[1].textContent;
+    spinner.className =  `${START_ANIMATION} ${INTRO_ANIMATION}`;
+    let texto = '',
+     text_parts = document.querySelectorAll('.j-ativo');
+    texto = `${text_parts[0].textContent}%20${text_parts[1].textContent}`;
     texto = texto.toLowerCase();
     console.log(texto);
     fetch('https://script.google.com/macros/s/AKfycbxvPUZTWBm5aE8_EZSM_CI8QxhkUm70scZYtidZ2XJYd91_U6r5B_iTZC35DoYIU4Ro/exec?text='+texto,{redirect: "follow",method: "GET"})
@@ -93,6 +92,6 @@ import { TERMS } from './TERMS.js';
     .then((response) => {
       console.log(response)
       document.querySelector('#traduzido').textContent = response.text;
-      spinner.className =  `${animationStart} ${exitAnimation}`;
+      spinner.className =  `${START_ANIMATION} ${EXIT_ANIMATION}`;
     });
   });
